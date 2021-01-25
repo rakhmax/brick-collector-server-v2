7a1538bdae2838150af334a7ffc0cc9ef44b87be
Maxim Rakhmanin <rakhmaxdev@gmail.com>
2021-01-25 23:39:07 +0300

    Add move to collection function

--- src/controllers/wishlist/patch.js
+++ src/controllers/wishlist/patch.js
@@ -1,3 +1,4 @@
+import { Types } from 'mongoose'
 import { Minifigure, Set } from '../../models'
 
 export default async (ctx) => {
@@ -14,9 +15,52 @@ export default async (ctx) => {
       )
     }
     if (type === 'S') {
-      res = Set.findOneAndUpdate(
+      const subsetFromBL = await bricklink.getItemSubset('Set', itemId)
+
+      const minifigsFromBL = subsetFromBL.filter(({ entries }) => {
+        return entries[0].item.type === 'MINIFIG' && !entries[0].is_counterpart
+      })
+      
+      const minifigsPromises = minifigsFromBL.map(({ entries }) => {
+        return bricklink.getCatalogItem('Minifig', entries[0].item.no)
+      })
+
+      const minifigs = (await Promise.all(minifigsPromises)).map((minifig) => {
+        return {
+          _id: Types.ObjectId(),
+          itemId: minifig.no,
+          userId: ctx.state.user.userId,
+          name: minifig.name,
+          categoryId: minifig.category_id,
+          year: minifig.year_released,
+          qty: minifigsFromBL.find(({ entries }) => entries[0].item.no === minifig.no).entries[0].quantity,
+        }
+      })
+
+      const minifigsRes = []
+
+      for (let minifig of minifigs) {
+        const minifigExists = await Minifigure.findOne({ itemId: minifig.itemId, userId: ctx.state.user.userId })
+
+        if (minifigExists) {
+          minifigsRes.push(Minifigure.findOneAndUpdate(
+            { itemId: minifig.itemId, userId: ctx.state.user.userId },
+            { $inc: { qty: minifig.qty } },
+            { new: true }
+          ))
+        } else {
+          minifigsRes.push(Minifigure.create(minifig))
+        }
+      }
+
+      await Promise.all(minifigsRes)
+
+      res = await Set.findOneAndUpdate(
         { userId: ctx.state.user.userId, itemId },
-        { inWishlist: false },
+        { 
+          minifigures: minifigs.map(({ itemId, qty }) => ({ qty, itemId })),
+          $unset: { inWishlist: 1 }
+        },
         { new: true }
       )
     }
