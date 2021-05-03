c621e75c442fd44a873bedcb05a715cd9d834a5c
Maxim Rakhmanin <rakhmaxdev@gmail.com>
2021-05-03 14:58:38 +0300

    gssss

--- src/controllers/auth/checkKey.js
+++ src/controllers/auth/checkKey.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 
 const checkKey = async (ctx) => {
   try {
-    const r = await axios.get('https://brickset.com/api/v3.asmx/checkKey?apiKey=3-akLS-e1Dz-E9BP2')
+    const { data } = await axios.get('https://brickset.com/api/v3.asmx/checkKey?apiKey=3-akLS-e1Dz-E9BP2')
     
     ctx.body = {
-      r
+      data
     }
   } catch (error) {
     ctx.throw(error.status, error.message)
